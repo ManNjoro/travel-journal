d91006aaf892507bdafd0569f9d9b898e44c7286
ManNjoro <elijohnmwoho@gmail.com>
2023-05-29 17:48:44 +0300

    adding image links

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import{FaMapMarkerAlt}  from "react-icons/fa"
 export default function Card(props) {
     return (
         <div className="card">
-            <img src={`../images/${props.img}`} alt="Nairobi city" className="img"></img>
+            <img src={props.img} alt="city" className="img"></img>
             <div className="card-content">
             <span className="location"><FaMapMarkerAlt color="orange"/>{props.location} <a href={props.link} className="link">VIEW ON GOOGLE MAPS</a></span>
                 <span className="title"><h1>{props.title}</h1></span>
--- src/components/data.js
+++ src/components/data.js
@@ -2,9 +2,9 @@
 export default [
     {
         id: 0,
-        img: "nairobi.jpeg",
+        img: "https://media.tacdn.com/media/attractions-splice-spp-674x446/07/5c/52/bc.jpg",
         location: "KENYA",
-        link: '',
+        link: 'https://goo.gl/maps/Npxf9WNeKWq8K5R7A',
         title: 'Nairobi city',
         startDate: "20 Jan 2023",
         endDate: "28 April 2023",
@@ -12,9 +12,9 @@ export default [
     },
     {
         id: 1,
-        img: "kisumu.jpeg",
+        img: "https://images.hivisasa.com/1200/G7Xtvr0OxfLake-Victoria-fish.jpg",
         location: "KENYA",
-        link: '',
+        link: 'https://goo.gl/maps/3Lgv5pUbyGe83CuJ9',
         title: 'Lake Victoria',
         startDate: "15 May 2021",
         endDate: "23 May 2021",
@@ -22,9 +22,9 @@ export default [
     },
     {
         id: 2,
-        img: "malindi.jpeg",
+        img: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/11/ff/d4/cb/mbabamb-3-largejpg.jpg?w=600&h=400&s=1",
         location: "KENYA",
-        link: '',
+        link: 'https://goo.gl/maps/xsbsPgZpm2e6Xuyz5',
         title: 'Malindi Beach',
         startDate: "15 Aug 2016",
         endDate: "23 Aug 2016",
